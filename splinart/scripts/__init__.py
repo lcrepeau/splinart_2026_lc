3c1bdca9dc28102d02dcad4a28ff9281dfa09676
LAURENT CREPEAU <laurent.crepeau@lmd.polytechnique.fr>
2026-06-19 11:07:27 +0200

    Ajout de splinart/scripts/__init__.py.

---
+++ splinart/scripts/__init__.py
@@ -0,0 +1,8 @@
+"""Game and player management package.
+
+This package provides classes for managing players and games.
+"""
+# Author:
+#     Loic Gouarin <loic.gouarin@gmail.com>
+#
+# License: BSD 3 clause

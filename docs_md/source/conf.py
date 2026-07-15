5752d84f2392b34e4d86ff655881ac43b0bce032
LAURENT CREPEAU <laurent.crepeau@lmd.polytechnique.fr>
2026-07-15 17:53:25 +0200

    docs: add import,path to import cleanly modules in docs_md/source/conf.py.

--- docs_md/source/conf.py
+++ docs_md/source/conf.py
@@ -1,3 +1,12 @@
+"""Configuration file for the Sphinx documentation builder."""
+
+from pathlib import Path
+import sys
+
+sys.path.insert(0, str(Path(__file__).resolve().parents[1] / "src"))
+import splinart
+print("Using splinart from:", splinart.__file__)
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -14,12 +23,13 @@ release = "0.1.0"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [ "sphinx.ext.autodoc",
+extensions = [
+    "sphinx.ext.autodoc",
     "numpydoc",
     "myst_parser",
     "nbsphinx",
     "nbsphinx_link",
-        ]
+]
 
 templates_path = ["_templates"]
 exclude_patterns = ["**.ipynb_checkpoints"]

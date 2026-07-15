e74330bcb8994ffe238530b08a94d0a28be2e614
LAURENT CREPEAU <laurent.crepeau@lmd.polytechnique.fr>
2026-07-15 15:06:05 +0200

    fix: add undeclared functions in src/splinart/__init__.py.

--- src/splinart/__init__.py
+++ src/splinart/__init__.py
@@ -4,8 +4,20 @@
 # License: BSD 3 clause
 """Splinart package."""
 
-from .compute import update_img
+from .compute import update_img, update_path
 from .draw import save_img, show_img
+from .scripts.cli_splinart import main
 from .shapes import circle, line
+from .spline import spline, splint
 
-__all__ = ["circle", "line", "save_img", "show_img", "update_img"]
+__all__ = [
+    "circle",
+    "line",
+    "main",
+    "save_img",
+    "show_img",
+    "spline",
+    "splint",
+    "update_img",
+    "update_path",
+]

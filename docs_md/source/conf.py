3c90f18ce3cd0641e9a72084285762bbdf03d8e5
LAURENT CREPEAU <laurent.crepeau@lmd.polytechnique.fr>
2026-07-15 18:00:58 +0200

    fix: adjust path in docs_md/source/conf.py.

--- docs_md/source/conf.py
+++ docs_md/source/conf.py
@@ -3,7 +3,8 @@
 from pathlib import Path
 import sys
 
-sys.path.insert(0, str(Path(__file__).resolve().parents[1] / "src"))
+sys.path.insert(0, str(Path(__file__).resolve().parents[2] / "src"))
+
 import splinart
 print("Using splinart from:", splinart.__file__)
 
